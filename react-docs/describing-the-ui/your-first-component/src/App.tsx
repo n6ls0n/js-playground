c74ca584ac2d5a0147d3bfb83f86f36d494c45a3
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-12-12 17:02:10 -0500

    Added describing-the-ui/your-first-component/

---
+++ react-docs/describing-the-ui/your-first-component/src/App.tsx
@@ -0,0 +1,83 @@
+import { JSX } from 'react/jsx-runtime';
+
+// ############# Defining a component #############
+
+// export default function Profile(): JSX.Element {
+//   return (
+//     <img
+//       src="https://i.imgur.com/MK3eW3Am.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   )
+// }
+
+// ############# Using a component #############
+
+// function Profile(): JSX.Element {
+//   return (
+//     <img
+//       src="https://i.imgur.com/MK3eW3As.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   );
+// }
+
+// export default function Gallery(): JSX.Element {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+// ############# Challenge 1 Solution #############
+
+// export default function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/lICfvbD.jpg"
+//       alt="Aklilu Lemma"
+//     />
+//   );
+// }
+
+// ############# Challenge 2 Solution #############
+
+// export default function Profile() {
+//   return(
+//     <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />
+//   );
+// }
+
+
+// ############# Challenge 3 Solution #############
+
+// function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/QIrZWGIs.jpg"
+//       alt="Alan L. Hart"
+//     />
+//   );
+// }
+
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+// ############# Challenge 4 Solution #############
+// Write a Congratulations component that shows <h1>Good job!</h1>. Don’t forget to export it!
+
+// export default function Congratulations() {
+//   return (<h1>Good job!</h1>);
+// }
