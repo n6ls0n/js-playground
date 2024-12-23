c55372617cf0b11afbc20d7d13be00e1cf39ab26
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-12-23 15:53:20 -0330

    Added describing-the-ui/writing-markup-with-jsx

---
+++ react-docs/describing-the-ui/writing-markup-with-jsx/src/App.tsx
@@ -0,0 +1,38 @@
+import { JSX } from 'react/jsx-runtime';
+
+// ############# Pro-tip: Use a JSX Converter #############
+
+// export default function TodoList() {
+//     return (
+//         <>
+//         <h1> Hedy Lamar's Todos </h1>
+//         <img
+//         src="https://i.imgur.com/yXOvdOSs.jpg"
+//         alt="Hedy Lamarr"
+//         className= "photo"
+//         />
+//         <ul>
+//             <li>Invent new traffic lights</li>
+//             <li> Rehearse a movie scene </li>
+//             <li> Improve the spectrum technology </li>
+//         </ul>
+//         </>
+//     )
+// }
+
+// ############# Challenge 1 Solution #############
+
+export default function Bio() {
+    return (
+        <>
+      <div className="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p className="summary">
+        You can find my thoughts here.
+        <br/>
+        <b>And <i/>pictures</b> of scientists!
+      </p>
+      </>
+    );
+}
