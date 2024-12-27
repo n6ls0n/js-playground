2c301609fdc8bc49cbb5ca1ace80daec9d64803f
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-12-27 19:06:01 -0330

    Changed file extension to allow for common js syntax use

--- https-server/webpack.config.cjs
+++ https-server/webpack.config.cjs
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.ts',
