e27cd23882255827d60892b5197f0ef9e5292916
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-12-28 11:24:01 -0330

    Added code to make the io module globally available

--- sockets-server/src/client/index.ts
+++ sockets-server/src/client/index.ts
@@ -1,1 +1,11 @@
-console.log("Client")
+import { io } from 'socket.io-client';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io('https://localhost:3000'); // Replace with your server URL
+
+    console.log('Socket.IO client initialized');
+
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+  });
