7d362f4b0da9679f05f79bb36faf2238d44c9bf5
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2025-01-06 17:18:37 -0330

    Added code for /adding-interactivity/state-as-a-snapshot

---
+++ react-docs/adding-interactivity/state-as-a-snapshot/src/App.tsx
@@ -0,0 +1,128 @@
+import { JSX } from 'react/jsx-runtime';
+
+// ############# Setting state triggers renders  #############
+
+//  See website
+
+// ############# Rendering takes a snapshot in time #############
+
+// import { useState } from 'react';
+
+// export default function Counter() {
+//   const [number, setNumber] = useState(0);
+
+//   return (
+//     <>
+//       <h1>{number}</h1>
+//       <button onClick={() => {
+//         setNumber(number + 1);
+//         setNumber(number + 1);
+//         setNumber(number + 1);
+//       }}>+3</button>
+//     </>
+//   )
+// }
+
+
+// ############# State over time #############
+
+// import { useState } from 'react';
+
+// export default function Counter() {
+//   const [number, setNumber] = useState(0);
+
+//   return (
+//     <>
+//       <h1>{number}</h1>
+//       <button onClick={() => {
+//         setNumber(number + 5);
+//         alert(number);
+//       }}>+5</button>
+//     </>
+//   )
+// }
+
+
+
+// #############
+
+// import { useState } from 'react';
+
+// export default function Counter() {
+//   const [number, setNumber] = useState(0);
+
+//   return (
+//     <>
+//       <h1>{number}</h1>
+//       <button onClick={() => {
+//         setNumber(number + 5);
+//         setTimeout(() => {
+//           alert(number);
+//         }, 3000);
+//       }}>+5</button>
+//     </>
+//   )
+// }
+
+
+
+// #############
+
+// import { useState } from 'react';
+
+// export default function Form() {
+//   const [to, setTo] = useState('Alice');
+//   const [message, setMessage] = useState('Hello');
+
+//   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//     setTimeout(() => {
+//       alert(`You said ${message} to ${to}`);
+//     }, 5000);
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         To:{' '}
+//         <select
+//           value={to}
+//           onChange={e => setTo(e.target.value)}>
+//           <option value="Alice">Alice</option>
+//           <option value="Bob">Bob</option>
+//         </select>
+//       </label>
+//       <textarea
+//         placeholder="Message"
+//         value={message}
+//         onChange={e => setMessage(e.target.value)}
+//       />
+//       <button type="submit">Send</button>
+//     </form>
+//   );
+// }
+
+// ############# Challenge 1 Solution #############
+import { useState } from 'react';
+
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
+  function handleClick() {
+    alert(walk ? 'Stop is Next' : 'Walk is Next');
+    setWalk(!walk);
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
+  );
+}
