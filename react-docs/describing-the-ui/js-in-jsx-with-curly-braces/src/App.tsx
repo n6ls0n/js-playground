4ea02fb8302812126f1b2fe4f71c5b88f87d4e00
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2025-01-06 10:34:12 -0330

    Added files for describing-the-ui/js-in-jsx-with-curly-braces

---
+++ react-docs/describing-the-ui/js-in-jsx-with-curly-braces/src/App.tsx
@@ -0,0 +1,169 @@
+import { JSX } from 'react/jsx-runtime';
+
+// ############# Passing strings with quotes #############
+// export default function Avatar() {
+//   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+//   const description = 'Gregorio Y. Zara';
+//   return (
+//     <img
+//       className="avatar"
+//       src={avatar}
+//       alt={description}
+//     />
+//   );
+// }
+// ############# Using curly braces:... #############
+// export default function TodoList() {
+//   const name = 'Gregorio Y. Zara';
+//   return (
+//     <h1>{name}'s To Do List</h1>
+//   );
+// }
+
+// #############
+
+// const today = new Date();
+
+// function formatDate(date: Date) {
+//   return new Intl.DateTimeFormat(
+//     'en-US',
+//     { weekday: 'long' }
+//   ).format(date);
+// }
+
+// export default function TodoList() {
+//   return (
+//     <h1>To Do List for {formatDate(today)}</h1>
+//   );
+// }
+
+// ############# Using “double curlies”: CSS... #############
+
+// export default function TodoList() {
+//   return (
+//     <ul style={{
+//       backgroundColor: 'black',
+//       color: 'pink'
+//     }}>
+//       <li>Improve the videophone</li>
+//       <li>Prepare aeronautics lectures</li>
+//       <li>Work on the alcohol-fuelled engine</li>
+//     </ul>
+//   );
+// }
+
+// ############# More fun with JavaScript...  #############
+
+// const person = {
+//   name: 'Gregorio Y. Zara',
+//   theme: {
+//     backgroundColor: 'black',
+//     color: 'pink'
+//   }
+// };
+
+// export default function TodoList() {
+//   return (
+//     <div style={person.theme}>
+//       <h1>{person.name}'s Todos</h1>
+//       <img
+//         className="avatar"
+//         src="https://i.imgur.com/7vQD0fPs.jpg"
+//         alt="Gregorio Y. Zara"
+//       />
+//       <ul>
+//         <li>Improve the videophone</li>
+//         <li>Prepare aeronautics lectures</li>
+//         <li>Work on the alcohol-fuelled engine</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// ############# Challenge 1 Solution #############
+
+// const person = {
+//   name: 'Gregorio Y. Zara',
+//   theme: {
+//     backgroundColor: 'black',
+//     color: 'pink'
+//   }
+// };
+
+// export default function TodoList() {
+//   return (
+//     <div style={person.theme}>
+//       <h1>{person.name}'s Todos</h1>
+//       <img
+//         className="avatar"
+//         src="https://i.imgur.com/7vQD0fPs.jpg"
+//         alt="Gregorio Y. Zara"
+//       />
+//       <ul>
+//         <li>Improve the videophone</li>
+//         <li>Prepare aeronautics lectures</li>
+//         <li>Work on the alcohol-fuelled engine</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// ############# Challenge 2 Solution #############
+
+// const person = {
+//   name: 'Gregorio Y. Zara',
+//   theme: {
+//     backgroundColor: 'black',
+//     color: 'pink'
+//   },
+//   url:"https://i.imgur.com/7vQD0fPs.jpg"
+// };
+
+// export default function TodoList() {
+//   return (
+//     <div style={person.theme}>
+//       <h1>{person.name}'s Todos</h1>
+//       <img
+//         className="avatar"
+//         src={person.url}
+//         alt="Gregorio Y. Zara"
+//       />
+//       <ul>
+//         <li>Improve the videophone</li>
+//         <li>Prepare aeronautics lectures</li>
+//         <li>Work on the alcohol-fuelled engine</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// ############# Challenge 3 Solution #############
+
+const baseUrl = 'https://i.imgur.com/';
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 's',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={baseUrl + person.imageId + person.imageSize + '.jpg'}
+        alt={person.name}
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
